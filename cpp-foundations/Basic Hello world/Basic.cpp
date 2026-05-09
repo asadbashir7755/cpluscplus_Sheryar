eab10528a2fb4802905411ef44a9cb79e386f8f2
Sheryar Ahsen Qamar <sheryarahsen.dev@gmail.com>
2026-05-10 00:57:22 +0500

    Initialpush allcode (#3)
    * pushing the previous code
    
    * contain initiall push all code
    
    * pushing previous code
    
    * adding the new changes
    
    * adding new changes second time
    
    * deleted folder
    
    * second push
    
    ---------
    
    Co-authored-by: AsadBashir <116044678+asadbashir7755@users.noreply.github.com>

--- cpp-foundations/Basic Hello world/Basic.cpp
+++ cpp-foundations/Basic Hello world/Basic.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     // This is my first cpp-foundation program...
-    cout << "Hello, World!" << endl;
+    cout << "Hello, World!......." << endl;
 
     return 0;
 }
